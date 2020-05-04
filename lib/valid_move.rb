a1d73163af9be0e18a53252f3fbb44b4309eef13
Ryan Locascio <loc.fitness1@gmail.com>
2020-05-04 15:29:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,3 +6,16 @@ def position_taken?(board, index)
     return true
   end
 end# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
